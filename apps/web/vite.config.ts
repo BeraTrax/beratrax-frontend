499e464620fd58808b3900f3ef23bde25389b299
MSamiTariq <samitariq40@gmail.com>
2025-03-26 14:57:09 +0500

    chore: update vite.config to add support for process.env in vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,45 +1,55 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    nodePolyfills({
-      globals: {
-        Buffer: true,
-        process: true,
-        global: true,
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on mode (e.g., development, production)
+  const env = loadEnv(mode, process.cwd(), "EXPO_PUBLIC_");
+
+  return {
+    plugins: [
+      react(),
+      svgr(),
+      nodePolyfills({
+        globals: {
+          Buffer: true,
+          process: true,
+          global: true,
+        },
+        protocolImports: true,
+      }),
+    ],
+    server: {
+      port: 3000,
+      open: true,
+    },
+    envPrefix: "EXPO_PUBLIC_",
+    resolve: {
+      alias: {
+        src: "/src",
+        "@core": "/../../packages/core/src",
+        jsbi: path.resolve(__dirname, "./node_modules/jsbi/dist/jsbi-cjs.js"),
+        "~@fontsource/ibm-plex-mono": "@fontsource/ibm-plex-mono",
+        "~@fontsource/inter": "@fontsource/inter",
+        "react-native": "react-native-web",
       },
-      protocolImports: true,
-    }),
-  ],
-  server: {
-    port: 3000,
-    open: true,
-  },
-  envPrefix: "REACT_APP_",
-  resolve: {
-    alias: {
-      src: "/src",
-      "@core": "/../../packages/core/src",
-      jsbi: path.resolve(__dirname, "./node_modules/jsbi/dist/jsbi-cjs.js"),
-      "~@fontsource/ibm-plex-mono": "@fontsource/ibm-plex-mono",
-      "~@fontsource/inter": "@fontsource/inter",
-      "react-native": "react-native-web",
     },
-  },
-  build: {
-    outDir: "build",
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    build: {
+      outDir: "build",
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
     },
-  },
-  optimizeDeps: {
-    exclude: ["./src/pages/Swap/uniswapTokens.json"],
-  },
+    optimizeDeps: {
+      exclude: ["./src/pages/Swap/uniswapTokens.json"],
+    },
+    define: {
+      "process.env": JSON.stringify(env),
+      "process.browser": true,
+    },
+  };
 });
+
--- packages/core/src/config/constants/index.ts
+++ packages/core/src/config/constants/index.ts
@@ -25,48 +25,49 @@ export const SNAPSHOT_GRAPHQL_URL = "https://hub.snapshot.org/graphql";
 export const MAX_GAS_UNITS_PER_TRANSACTION = "700000";
 export const defaultChainId = CHAIN_ID.BERACHAIN; // Berachain
 export const defaultNetworkName = "berachain";
-export const walletConnectProjectId = import.meta.env.REACT_APP_WALLET_CONNECT_PROJECT_ID as string;
-export const isDev = import.meta.env.NODE_ENV === "development";
+export const walletConnectProjectId = process.env.EXPO_PUBLIC_WALLET_CONNECT_PROJECT_ID as string;
+export const isDev = process.env.NODE_ENV === "development";
 export const isStagging = window.location.hostname.includes("staging.beratrax.com");
-export const GATEFI_MERCHANT_ID = import.meta.env.REACT_APP_GATEFI_MERCHANT_ID as string;
-export const SOCKET_BRIDGE_KEY = import.meta.env.REACT_APP_SOCKET_BRIDGE_KEY;
-export const SOCKET_API_KEY = import.meta.env.REACT_APP_SOCKET_BRIDGE_KEY;
-export const RAMP_TRANSAK_API_KEY = import.meta.env.REACT_APP_RAMP_TRANSAK_API_KEY;
-export const ZERODEV_PROJECT_ID = import.meta.env.REACT_APP_ZERODEV_PROJECT_ID!;
-export const ZERODEV_PROJECT_ID_MAINNET = import.meta.env.REACT_APP_ZERODEV_PROJECT_ID_MAINNET!;
-export const IS_LEGACY = import.meta.env.REACT_APP_IS_LEGACY === "true";
+export const GATEFI_MERCHANT_ID = process.env.EXPO_PUBLIC_GATEFI_MERCHANT_ID as string;
+export const SOCKET_BRIDGE_KEY = process.env.EXPO_PUBLIC_SOCKET_BRIDGE_KEY;
+export const SOCKET_API_KEY = process.env.EXPO_PUBLIC_SOCKET_BRIDGE_KEY;
+export const RAMP_TRANSAK_API_KEY = process.env.EXPO_PUBLIC_RAMP_TRANSAK_API_KEY;
+export const ZERODEV_PROJECT_ID = process.env.EXPO_PUBLIC_ZERODEV_PROJECT_ID!;
+export const ZERODEV_PROJECT_ID_MAINNET = process.env.EXPO_PUBLIC_ZERODEV_PROJECT_ID_MAINNET!;
+export const IS_LEGACY = process.env.EXPO_PUBLIC_IS_LEGACY === "true";
 export const DODO_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/dodoex/dodoex-v2-arbitrum";
 export const FRAX_APR_API_URL = "https://stargate.finance/.netlify/functions/farms";
 export const SUSHUISWAP_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/sushi-0m/sushiswap-arbitrum";
 export const SHUSHISWAP_CHEF_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/jiro-ono/arbitrum-minichef-staging";
 export const SWAPFISH_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/swapfish/swapfish";
-export const WEB3AUTH_CLIENT_ID = import.meta.env.REACT_APP_WEB3AUTH_CLIENT_ID;
+export const WEB3AUTH_CLIENT_ID = process.env.EXPO_PUBLIC_WEB3AUTH_CLIENT_ID;
 export const EARNINGS_GRAPH_URL =
   "https://api.goldsky.com/api/public/project_cm4bd2i5uaow101vy0y342dfg/subgraphs/beratrax-mainnet-subgraph/6.0.0/gn";
 export const EARNINGS_GRAPH_URL_BASE =
   "https://gateway-arbitrum.network.thegraph.com/api/616d6a1cc1199359a718e468c9aec235/subgraphs/id/D7uDmHS7qoxRwxHPnYNQm2foppkWmi7r2TaH5qZDX2Dh";
 export const HOP_EXCHANGE_APY_URL = "https://assets.hop.exchange/v1.1-pool-stats.json";
 export const BACKEND_BASE_URL = "https://api.beratrax.io/api/v1";
-export const TENDERLY_ACCESS_TOKEN = import.meta.env.REACT_APP_TENDERLY_ACCESS_TOKEN;
-export const TENDERLY_PROJECT_SLUG = import.meta.env.REACT_APP_TENDERLY_PROJECT_SLUG;
-export const TENDERLY_USER_NAME = import.meta.env.REACT_APP_TENDERLY_USER_NAME;
-export const WERT_PARTNER_ID = import.meta.env.REACT_APP_WERT_PARTNER_ID;
+export const TENDERLY_ACCESS_TOKEN = process.env.EXPO_PUBLIC_TENDERLY_ACCESS_TOKEN;
+export const TENDERLY_PROJECT_SLUG = process.env.EXPO_PUBLIC_TENDERLY_PROJECT_SLUG;
+export const TENDERLY_USER_NAME = process.env.EXPO_PUBLIC_TENDERLY_USER_NAME;
+export const WERT_PARTNER_ID = process.env.EXPO_PUBLIC_WERT_PARTNER_ID;
 export const POLLING_INTERVAL = 30000;
-export const INFURA_KEY = import.meta.env.REACT_APP_INFURA;
-export const ALCHEMY_KEY = import.meta.env.REACT_APP_ALCHEMY;
-console.log(import.meta.env.REACT_APP_BARTIO_RPC_URL);
+export const INFURA_KEY = process.env.EXPO_PUBLIC_INFURA;
+export const ALCHEMY_KEY = process.env.EXPO_PUBLIC_ALCHEMY;
+console.log(process.env.EXPO_PUBLIC_BARTIO_RPC_URL);
 export const BARTIO_RPC_URL =
-  import.meta.env.REACT_APP_BARTIO_RPC_URL ??
+  process.env.EXPO_PUBLIC_BARTIO_RPC_URL ??
   "https://berachain-bartio.g.alchemy.com/v2/VUwkyWR596J0Wq8sxvXtBYH_8X7CLcNn";
 
 // export const FRONT_URL = import.meta.env.REACT_APP_FRONT_URL_SANDBOX as string;
 // export const FRONT_API_KEY = import.meta.env.REACT_APP_FRONT_API_KEY_SANDBOX as string;
-export const FRONT_URL = import.meta.env.REACT_APP_FRONT_URL as string;
-export const FRONT_API_KEY = import.meta.env.REACT_APP_FRONT_API_KEY as string;
-export const FRONT_CLIENT_ID = import.meta.env.REACT_APP_FRONT_CLIENT_ID as string;
+export const FRONT_URL = process.env.EXPO_PUBLIC_FRONT_URL as string;
+export const FRONT_API_KEY = process.env.EXPO_PUBLIC_FRONT_API_KEY as string;
+export const FRONT_CLIENT_ID = process.env.EXPO_PUBLIC_FRONT_CLIENT_ID as string;
 export const REACT_APP_GA_MEASUREMENT_ID = "G-60G0YWYR4G";
 
 export const tenderlyRpcs: { [key: string]: string } = {
   [CHAIN_ID.ARBITRUM]: `https://arbitrum.gateway.tenderly.co/6YtRegY86MErUrbfeB4e8L`,
   [CHAIN_ID.BASE]: `https://base.gateway.tenderly.co/6lueMXUaDSXb1VtTVFELQ1`,
 };
+

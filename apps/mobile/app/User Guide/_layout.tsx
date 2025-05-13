34f51e3ddb0fb3e831a10b4709f0d02cdbdded23
musti17 <mustafazahid167@gmail.com>
2025-05-13 15:09:22 +0500

    chore: background color changed

--- apps/mobile/app/User Guide/_layout.tsx
+++ apps/mobile/app/User Guide/_layout.tsx
@@ -3,7 +3,7 @@ import { View, SafeAreaView } from "react-native";
 
 const UserGuide = () => {
 	return (
-		<SafeAreaView className="flex-1 bg-black">
+		<SafeAreaView className="flex-1 bg-bgSecondary">
 			<View className="flex-1 w-full overflow-hidden">
 				<UserGuideSharedComponent />
 			</View>
